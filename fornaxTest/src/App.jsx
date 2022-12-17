6e93137635d2d1d9b739243ca753c46f70476183
Tochukwu Jonah <jonahtochukwu@gmail.com>
2022-12-17 01:45:28 +0100

    Ckick to navigate

--- fornaxTest/src/App.jsx
+++ fornaxTest/src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+// import { useNavigate } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
+  // const navigate = useNavigate();
 
+
+  const visitPage = () =>{
+    window.location.href = "https://fornaxcloud.com/";
+  } 
 
   return (
     <div className="App">
       
       
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={visitPage}>
+          Click me to navigate
         </button>
        
       </div>
