a03a15b7aead416b68b493e29da9301d0dbad8cd
Tochukwu Jonah <31604841+tochukwujonah@users.noreply.github.com>
2022-12-21 11:51:26 +0100

    Updated axios request

--- fornaxTest/src/App.jsx
+++ fornaxTest/src/App.jsx
@@ -1,20 +1,40 @@
 import { useState } from 'react'
+import fornaxAxios from './axios/fornaxAxios';
 import './App.css'
 // import { useNavigate } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-  // const navigate = useNavigate();
-
-
+  
+  
   const visitPage = () =>{
     window.location.href = "https://fornaxcloud.com/";
   } 
 
+
+  // Attempt to login
+
+  const login = async () => {
+
+
+    const userData = {
+      userName: 'latte@sakalgb.com',
+      password: 'Pass1234',
+    };
+
+    // console.log(userData);
+    const response = await fornaxAxios.post("/library/auth/login", userData); 
+    
+    if(response.data){
+        // sessionStorage.setItem('user', JSON.stringify(response.data.data.token));
+        sessionStorage.setItem('authToken', JSON.stringify(response.data.data.token));
+    }
+    return response.data;
+    }
+
   return (
-    <div className="App">
+    <div className="App" >
       
-      
+      <iframe src="https://www.fornaxcloud.com" onLoad={login} frameborder="2" width={'1200px'} height='400px'></iframe>
       <div className="card">
         <button onClick={visitPage}>
           Click me to navigate
@@ -27,3 +47,21 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+// function postCrossDomainMessage(msg) {
+//   var win = document.getElementById('cookieValue').contentWindow; 
+//   win.postMessage(msg, "http://dev.lagoseppps.com.ng/");
+// } 
+//   var edCookie = document.cookie="sessionId=s%32AkSPun1jMN2Yx655DttwtQSN75D1cvWCP.tKA1v7Ap0z3vYicGjtn7DfV3L5e2EjsPlllzxHDZts0; SameSite=None;Secure; Path=/;";  
+//   var postMsg = edCookie; 
+  
+//   console.log (postMsg); 
+  
+//   localStorage.setItem("cookie", postMsg)
+
+  
+
--- fornaxTest/src/axios/fornaxAxios.js
+++ fornaxTest/src/axios/fornaxAxios.js
@@ -2,10 +2,56 @@ import axios from 'axios'
 
 export default axios.create({
    
-    baseURL: "dev.lagoseppps.com.ng",
+    baseURL: "https://fornaxqa.southeastasia.cloudapp.azure.com/fornaxcloudqa",
     headers: {
-      "Access-Control-Allow-Origin": "*", 
-      'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem(""))}`,
+      'Content-Type':'application/json',
+      "Access-Control-Allow-Origin": "http://127.0.0.1:5173", 
+      'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`,
       // 'Content-Type': 'multipart/form-data'
     },
   });
+
+
+
+
+  // const login = async (userData) => {
+
+
+  //   const userData = {
+  //     userName: '',
+  //     password: '',
+  //   };
+  //   const response = await fornaxAxios.post("/library/auth/login", userData); 
+    
+
+  //   if(response.data){
+  //       // sessionStorage.setItem('user', JSON.stringify(response.data.data.token));
+  //       sessionStorage.setItem('authToken', JSON.stringify(response.data.data.token));
+  //   }
+  //   return response.data;
+  //   }
+
+
+
+
+
+
+
+
+
+  // const response = await axios.get(
+  //   baseURL,
+  //   {headers: {
+  //           "Access-Control-Allow-Origin" : "*",
+  //           "Content-type": "Application/json",
+  //           'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem(""))}`,
+  //           }   
+  //       }
+  // )
+  // .then((response) => {
+  //     var response = response.data;
+  //   },
+  //   (error) => {
+  //     var status = error.response.status
+  //   }
+  // );
